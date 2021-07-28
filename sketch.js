81a572c5f030e576832dee2824ac1b148d0fc914
Divanshu1234 <86780556+Divanshu1234@users.noreply.github.com>
2021-07-28 16:04:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw(){
+
+  background("pink")
+  if(keyDown(UP_ARROW)){
+    background('green')
+  }
+  if(keyDown(DOWN_ARROW)){
+    background('YELLOW')
+  }
+} 
+
+   
+
+
+
+
+
+
